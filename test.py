0cb7149b54f98cba3f3e9b8c10db3aa021176fb5
kevinfang2 <kevkev0123@gmail.com>
2018-01-29 15:25:45 -0800

    gets link when without captcha

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -24,3 +25,14 @@ ActionChains(driver).move_to_element(mature_menu).click(mature_accept).perform()
 menu = driver.find_element_by_css_selector(".player-menu")
 hidden_submenu = driver.find_element_by_css_selector(".pl-clips-button")
 ActionChains(driver).move_to_element(menu).click(hidden_submenu).perform()
+
+html = driver.page_source
+soup = BeautifulSoup(html, "html5lib")
+form = soup.find("form", {"class" : "js-create-clip-form"})
+print (form)
+broadcast_ID = soup.find("input", {"class" : "js-create-clip-broadcast_id"})
+print (broadcast_ID)
+offset_time = soup.find("input", {"class" : "js-create-clip-offset"})
+print (offset_time)
+
+clip_url = "https://clips-media-assets.twitch.tv/raw_media/" + broadcast_ID + "-offset-" + offset_time + ".mp4#t=60"
